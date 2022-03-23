a5d200b7b249b38f545e81e4c07d90feba97280b
Putri Yasmin <putriyasmin438@gmail.com>
2022-03-23 10:04:01 +0800

    commit pertama

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "My Apps",
+    home: new Myapp(),
+  ));
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: new Text("My Apps"),
+        ),
+        body: new Container(
+            padding: EdgeInsets.all(20.0),
+            child: new Center(
+              child: new Text("Hello World"),
+            )));
+  }
+}
